1bdb479591f0cb5d40211f25553452cef7a84fb1
Gabriel <gluzjabar@gmail.com>
2024-02-29 15:06:46 -0300

    Commit inicial

---
+++ Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeFinal {
+    internal class Conta {
+
+        //ATRIBUTOS 
+        private string _nome;
+        private double _saldo;
+
+        //AUTO PROPERTIES
+        public int Numero { get; private set; }
+
+
+        //CONSTRUTORES
+        public Conta(int numeroConta, string nomeConta) {
+            Numero = numeroConta;
+            _nome = nomeConta;
+            _saldo = 0;
+        }
+
+        //PROPERTIES
+        public string Nome {
+            get { return _nome; }
+            set { if (value != null && value.Length > 4) {
+                    _nome = value;
+                } }
+        }
+
+        public double Saldo {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+
+
+        //MÉTODOS
+
+        //Adicionar saldo a conta
+        public void Deposito(double valorDeposito) {
+            Saldo = Saldo + valorDeposito;
+        }
+
+        //Remover saldo da conta
+        public void Saque(double valorSaque) {
+            Saldo = Saldo - (valorSaque + 5.00);
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace AtividadeFinal {
+    internal class Program {
+        static void Main(String[] args) {
+
+            Console.Write("Informe o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o titular da conta: ");
+            string nomeConta = Console.ReadLine();
+
+            Conta c = new Conta(numeroConta, nomeConta);
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string deposito = Console.ReadLine();
+            if (deposito == "s") {
+                Console.Write("Informe o valor do depósito inicial: ");
+                c.Saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Conta {c.Numero}, Titular: {c.Nome}, Saldo: $ {c.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.Write("Entre com um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(valorDeposito);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Conta {c.Numero}, Titular: {c.Nome}, Saldo: $ {c.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.Write("Entre com um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(valorSaque);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Conta {c.Numero}, Titular: {c.Nome}, Saldo: $ {c.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+
+
+
+        }
+    }
+}
